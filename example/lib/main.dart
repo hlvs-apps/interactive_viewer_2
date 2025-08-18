5caafd870c756fd2d9e054d3598bd7b1ba38119e
burhankhanzada <burhankhanzada199888@gmail.com>
2025-08-18 12:44:46 +0200

    feat: Added zoom and pan methods to easily transform using shorcut or button

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:interactive_viewer_2/interactive_viewer_2.dart';
 
 void main() {
@@ -14,11 +15,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = InteractiveViewer2Controller();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: InteractiveViewer2(child: FlutterLogo(size: 1000)));
+    return Scaffold(
+      body: CallbackShortcuts(
+        bindings: {
+          // Zooming
+          const SingleActivator(LogicalKeyboardKey.equal): zoomIn,
+          const SingleActivator(LogicalKeyboardKey.minus): zoomOut,
+          // Panning
+          const SingleActivator(LogicalKeyboardKey.arrowUp): panUp,
+          const SingleActivator(LogicalKeyboardKey.arrowDown): panDown,
+          const SingleActivator(LogicalKeyboardKey.arrowLeft): panLeft,
+          const SingleActivator(LogicalKeyboardKey.arrowRight): panRight,
+        },
+        child: Focus(
+          autofocus: true,
+          child: Stack(
+            children: [
+              InteractiveViewer2(
+                controller: controller,
+                maxScale: 1.5,
+                minScale: 0.5,
+                child: FlutterLogo(size: 1000),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(onPressed: zoomIn, icon: Icon(Icons.zoom_in)),
+                    IconButton(onPressed: zoomOut, icon: Icon(Icons.zoom_out)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  void zoomIn() => controller.zoomIn(1.1);
+  void zoomOut() => controller.zoomOut(0.9);
+
+  void panUp() => controller.pan(const Offset(0, 10));
+  void panDown() => controller.pan(const Offset(0, -10));
+  void panLeft() => controller.pan(const Offset(10, 0));
+  void panRight() => controller.pan(const Offset(-10, 0));
 }
